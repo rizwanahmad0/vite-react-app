96cffa54cce08317081e1751a4b087f7b47502a4
rizwanahmad0 <ra32509@gmail.com>
2024-12-14 23:25:31 +0530

    S

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <p>hi rizwan here ka be kr diya na lie</p>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
